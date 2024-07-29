3213089d9168e66662314a14d9e2bd2609de2510
katheer <[email protected]>
2024-07-29 06:25:16 +0300

    feat: Add API keys for secret and publishable keys

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,6 @@
+class ApiKeys {
+  static const String secretKey =
+      'sk_test_51PhTA2RokQ5PolAWJyOoi5p7bEwu3zp3pHhtBYNZJ9RL7mpbLK93EogZ1Oo4OMWWjcBWz8E8WuqGy1je8vfeK8Si00INzLpEZo';
+  static const String publishableKey =
+      'pk_test_51PhTA2RokQ5PolAWmHJaDK5TjZW1dYIp9QQYB98kEhuf7wOmHDZNDqOoyqaxQ2kJWeuN2Y0STVWkA4ZyUs6X7rrX00ctVozuhV';
+}
